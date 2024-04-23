ec0e43e9b579b069a69deb5e1380f36a459cd953
Sadia Imran <164052169+codewithsadia@users.noreply.github.com>
2024-04-23 23:01:05 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /user/bin/env/ node
+//This line is called a shabang, it tells the os to run it with Node.js
+
+//import the `inquirer` module, which is a command line interface for Node.js 
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//Display a colorfull welcome message
+console.log(chalk.bold.cyanBright("\n\t\t code with sadia-words counter"));
+console.log("=".repeat(60));
+
+//Prompt the user to enter a sentence
+let answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.green("Enter your sentence to count the word"),
+    }
+]);
+
+//Triming the sentence and splitting it into words based on "spaces"
+let words = answers.sentence.trim().split(" ");
+
+//Analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold("- sentence words:"));
+console.log(words);
+console.log(chalk.bold.magenta(`\n - word count: ${chalk.redBright(words.lenght) }`));
+console.log("=".repeat(60));
